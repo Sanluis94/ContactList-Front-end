0044e7daea6b965365410c5f39d94f86171fb373
Sanluis94 <luisadamski94@gmail.com>
2025-03-03 01:40:42 -0300

    Update method working finally

--- src/app/components/person-form/person-form.component.ts
+++ src/app/components/person-form/person-form.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PersonService } from '../../services/person.service';
+import { ContactService } from '../../services/contact.service'; // Importando o serviço de contatos
 import { Person } from '../../models/person';
+import { ContactDTO } from '../../models/contact-dto';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -14,10 +16,13 @@ export class PersonFormComponent implements OnInit {
   personForm: FormGroup;
   isEditing = false;
   personId?: number;
+  contacts: ContactDTO[] = [];
+  showContacts = false; 
 
   constructor(
     private fb: FormBuilder,
     private personService: PersonService,
+    private contactService: ContactService,
     private route: ActivatedRoute,
     private router: Router,
     private http: HttpClient
@@ -40,9 +45,26 @@ export class PersonFormComponent implements OnInit {
     if (this.personId) {
       this.isEditing = true;
       this.personService.getPersonById(this.personId).subscribe(person => {
-        this.personForm.reset(person); // Usa reset para garantir atualização correta
+        this.personForm.reset(person);
+        this.loadContacts();
       });
     }
+  }
+
+
+  loadContacts(): void {
+    if (this.personId) {
+      this.contactService.getContactsByPersonId(this.personId).subscribe((data: ContactDTO[]) => {
+        this.contacts = data;
+      });
+    }
+  }
+
+  toggleContacts(): void {
+    this.showContacts = !this.showContacts;
+  }
+
+  addContact(): void {
   }
 
   onSubmit(): void {
@@ -50,47 +72,37 @@ export class PersonFormComponent implements OnInit {
       this.personForm.markAllAsTouched();
       return;
     }
-
+  
     const person: Person = this.personForm.value;
     console.log('Enviando para a API:', JSON.stringify(person, null, 2));
-
-    if (this.isEditing) {
-      // Se estiver editando, faz um PUT
-      this.personService.updatePerson(this.personId!, person).subscribe({
-        next: () => {
-          console.log('Pessoa atualizada com sucesso:', person);
-          this.router.navigate(['/person-table']);
-        },
-        error: (err) => {
-          console.error('Erro ao atualizar pessoa:', err);
-        }
-      });
-    } else {
-      // Se não estiver editando, faz um POST para criar uma nova pessoa
-      this.personService.createPerson(person).subscribe({
-        next: () => {
-          console.log('Pessoa criada com sucesso:', person);
-          this.router.navigate(['/person-table']);
-        },
-        error: (err) => {
-          console.error('Erro ao criar pessoa:', err);
-        }
-      });
-    }
-  }
-
-  fetchAddress(): void {
-    const zipCode = this.personForm.get('zipCode')?.value;
-    if (zipCode.length === 8) {
-      this.http.get<any>(`https://viacep.com.br/ws/${zipCode}/json/`).subscribe(data => {
-        if (!data.erro) {
-          this.personForm.patchValue({
-            address: `${data.logradouro}, ${data.bairro}`,
-            city: data.localidade,
-            state: data.uf
-          });
-        }
-      });
+  
+    const confirmation = window.confirm('Tem certeza que deseja atualizar esta pessoa?');
+    
+    if (confirmation) {
+      if (this.isEditing) {
+        this.personService.updatePerson(this.personId!, person).subscribe({
+          next: (response) => {
+            console.log('Pessoa atualizada com sucesso:', response);
+            alert('Pessoa atualizada com sucesso!');
+            this.router.navigate(['/person-table']);
+          },
+          error: (err) => {
+            console.error('Erro ao atualizar pessoa:', err);
+            alert('Erro ao atualizar pessoa. Tente novamente.');
+          }
+        });
+      } else {
+        this.personService.createPerson(person).subscribe({
+          next: () => {
+            console.log('Pessoa criada com sucesso:', person);
+            this.router.navigate(['/person-table']);
+          },
+          error: (err) => {
+            console.error('Erro ao criar pessoa:', err);
+            alert('Erro ao criar pessoa. Tente novamente.');
+          }
+        });
+      }
     }
   }
 
--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Person } from '../models/person';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
-  private apiUrl = 'http://localhost:8081/api/people';
+  private apiUrl = 'http://localhost:8080/api/people';
 
   constructor(private http: HttpClient) {}
 
@@ -24,8 +26,17 @@ export class PersonService {
   }
 
   updatePerson(id: number, person: Person): Observable<Person> {
-    return this.http.put<Person>(`${this.apiUrl}/${id}`, person);
+    return this.http.put<Person>(`${this.apiUrl}/${id}`, person).pipe(
+      tap(response => {
+        console.log('Resposta do PUT:', response); 
+      }),
+      catchError(error => {
+        console.error('Erro ao atualizar pessoa:', error);
+        return throwError(error); 
+      })
+    );
   }
+  
 
   deletePerson(id: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/${id}`);
